fa6eee712792c1909b194eee867585f5abdbd70d
Sevde Aydin <sevdenuraydn7@gmail.com>
2023-09-21 22:49:34 +0300

    Initial Commit

---
+++ MovieBook/MovieBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieBookApp.swift
+//  MovieBook
+//
+//  Created by Sevde Aydın on 21.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct MovieBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
